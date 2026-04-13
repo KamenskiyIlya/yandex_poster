6b1c687facc12231ee4323296382a43ee8b47f55
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-04-13 20:38:37 +0300

    Add blank start page

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from where_to_go import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.start_page)
 ]

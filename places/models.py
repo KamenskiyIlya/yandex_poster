cbc54d4ebcd85c1d7a31f164ae40f455a3b7892f
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-04-14 23:05:45 +0300

    Configured views with using database

--- places/models.py
+++ places/models.py
@@ -5,8 +5,9 @@ class Place(models.Model):
     title = models.CharField(verbose_name='Название')
     description_short = models.CharField(max_length=300, verbose_name='Короткое описание')
     description_long = models.TextField(verbose_name='Полное описание')
-    latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Долгота')
+    latitude = models.FloatField(verbose_name='Широта')
+    
     
     def __str__(self):
         return self.title

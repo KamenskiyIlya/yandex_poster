ac21d896e9e471b0b70fceb7685f86c7b1535fc0
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-04-15 21:59:08 +0300

    Made title field clickable

--- places/admin.py
+++ places/admin.py
@@ -5,6 +5,7 @@ from .models import Place, PlaceImage
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
     ordering = ['id']
 
 

85c390a57afdf636eb0c5fca9975048f562d8333
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-04-15 22:22:49 +0300

    Setup inline display for adding photos

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
     list_display_links = ['id', 'title']
     ordering = ['id']
+    inlines = [PlaceImageInline]
 
 
 admin.site.register(PlaceImage)

1eef263e6ada9fc7b747b02fb24aeb76a70c0843
KamenskiyIlya <kamenskj.ilya@gmail.com>
2026-04-27 21:08:23 +0300

    Made discription fields not strictly necessary

--- places/models.py
+++ places/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Place(models.Model):
     title = models.CharField(verbose_name='Название', unique=True)
     description_short = models.CharField(
-        max_length=300, verbose_name='Короткое описание'
+        max_length=300, verbose_name='Короткое описание', blank=True
     )
-    description_long = models.TextField(verbose_name='Полное описание')
+    description_long = models.TextField(
+        verbose_name='Полное описание', blank=True
+    )
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
 
